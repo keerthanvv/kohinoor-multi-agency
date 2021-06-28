c6383e263e6aa64fda83ab8740391455595ccc2a
keerthan <keerthanbilla1994@gmail.com>
2021-06-28 23:05:21 +0530

    test push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
@@ -18,5 +20,10 @@ export class LoginComponent implements OnInit {
   }
 onSubmit(){
   console.log("form submited");
+  if((this.credentials.username!='' && this.credentials.password!='') && (this.credentials.username!=null &&this.credentials.password!=null)){    
+  }else{
+    console.log("wrong creds");
+    Swal.fire('Any of the Login fields can not be empty.Please try again!!')
+  }
 }
 }
